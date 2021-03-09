7ca0359b290d2e7d5e3047c4bf144a167fabb9fa
Jeferson Cavalcante <jeferson.mdc@hotmail.com>
2021-03-08 22:04:36 -0300

    update project

--- version2/js/script.js
+++ version2/js/script.js
@@ -5,22 +5,18 @@ function carregaJSON(filename) {
     dados = data})
     .catch(error => console.error(error));
 }
-
 function adicionaLinha(){
     document.getElementById("imagens").innerHTML += `<div class="row" id="linha"></div>`
 }
 
 function adicionaColuna(Id,Url) {
-    document.getElementById("linha").innerHTML += `<div class="col-3"><img id="${Id}" src="${Url}" alt="" ></div>`;
+    document.getElementById(`linha`).innerHTML += `<div class="col-3"><img id="${Id}" src="${Url}" alt="" ></div>`;
 }
 carregaJSON('json/imagens.json');
-
 function Teste(){
-    for (i = 0; i < 3; i++) {
-        adicionaLinha();
+    adicionaLinha();
+    for (i = 0; i < 12; i++) {
         adicionaColuna(dados[i].Id, dados[i].Url);
-        adicionaColuna(dados[i].Id, dados[i].Url);
-        adicionaColuna(dados[i].Id, dados[i].Url);
-        adicionaColuna(dados[i].Id, dados[i].Url);
-     }
+    }
+
 }
