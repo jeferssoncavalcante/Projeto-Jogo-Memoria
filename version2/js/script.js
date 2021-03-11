d162819758c7fb7bf0cefa1883c11035a9bbcbd3
Jeferson Cavalcante <jeferson.mdc@hotmail.com>
2021-03-11 20:13:14 -0300

    update project

--- version2/js/script.js
+++ version2/js/script.js
@@ -5,29 +5,46 @@ function carregaJSON(filename) {
     dados = data})
     .catch(error => console.error(error));
 }
+function limpaPagina(){
+    document.getElementById("memoria").innerHTML = ``;
+}
 function adicionaDivImagens(){
-    document.getElementById("memoria").innerHTML += `<div id="imagens"></div>`;
+    document.getElementById("memoria").innerHTML += `<div id="imagens"><div id="cLol" class="card-header bg-primary text-white">League of Legends</div></div>`;
 }
 function adicionaLinha(){
     document.getElementById("imagens").innerHTML += `<div class="row" id="linha"></div>`;
 }
-function adicionaColuna(Id,Url) {
-    document.getElementById(`linha`).innerHTML += `<div class="col-3"><img class="front-face" id="${Id}" src="${Url}" alt="Face da Carta" ></div>`;
+function adicionaColuna(Id,Url,col) {
+    document.getElementById(`linha`).innerHTML += `<div class="col-${col}"><img class="front-face" id="${Id}" src="${Url}" alt="Face da Carta" ></div>`;
 }
 carregaJSON('json/imagens.json');
+var select = document.getElementById('dificuldade');
+var value = select.options[select.selectedIndex].value;
+/*select.addEventListener('change', function(){
+    console.log(select.value)
+})*/
 function Teste(){
+    limpaPagina();
     adicionaDivImagens();
     adicionaLinha();
+if (select.value == 500) {
     for (i = 0; i < 6; i++) {
-        var num = Math.floor(Math.random() * 50);
-        adicionaColuna(dados[num].Id, dados[num].Url);
-        adicionaColuna(dados[num].Id, dados[num].Url);
+            var num = Math.floor(Math.random() * 50);
+            adicionaColuna(dados[num].Id, dados[num].Url,3);
+            adicionaColuna(dados[num].Id, dados[num].Url,3);
+        }
     }
+else if (select.value == 600) {
+        for (i = 0; i < 12; i++) {
+            var num = Math.floor(Math.random() * 50);
+            adicionaColuna(dados[num].Id, dados[num].Url,3);
+            adicionaColuna(dados[num].Id, dados[num].Url,3);
+        }
+    } else {
+        for (i = 0; i < 24; i++) {
+            var num = Math.floor(Math.random() * 50);
+            adicionaColuna(dados[num].Id, dados[num].Url,3);
+            adicionaColuna(dados[num].Id, dados[num].Url,3);
+        }
+    }   
 }
-/*const cards = document.querySelectorAll('.col-3');
-
-function flipCard() {
-  this.classList.toggle('flip');
-}
-
-cards.forEach(card => card.addEventListener('click', flipCard));*/
