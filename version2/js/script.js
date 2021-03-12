8ccb12be2754760b7873c0560ca78f636704defe
Jeferson Cavalcante <jeferson.mdc@hotmail.com>
2021-03-12 11:46:02 -0300

    update project

--- version2/js/script.js
+++ version2/js/script.js
@@ -14,44 +14,41 @@ function adicionaDivImagens(){
 function adicionaLinha(){
     document.getElementById("imagens").innerHTML += `<div class="row" id="linha"></div>`;
 }
-function adicionaColuna(Id,Url,col) {
-    document.getElementById(`linha`).innerHTML += `<div class="col-${col}"><img class="front-face" id="${Id}" src="${Url}" alt="Face da Carta" ></div>`;
+function adicionaColuna(Id,Url) {
+    document.getElementById(`linha`).innerHTML += `<div class="col-3"><img class="front-face" id="${Id}" src="${Url}" alt="Face da Carta" ></div>`;
 }
-/*var select1 = document.getElementById('tema');
-var value1 = select1.options[select1.selectedIndex].value1;
-select1.addEventListener('change', function(){
-    console.log(select1.value1)
-})*/
-/*if (select1.value1 == 238) {*/
-carregaJSON('json/imagensLol.json');
-/*}*/
+var temas = document.getElementById("tema");
+
 var select = document.getElementById('dificuldade');
-var value = select.options[select.selectedIndex].value;
-/*select.addEventListener('change', function(){
-    console.log(select.value)
-})*/
+temas.addEventListener('change',function carregaTema(){
+    if (temas.selectedIndex == 1){
+        carregaJSON('json/imagensPokemon.json');
+    } else {
+        carregaJSON('json/imagensLol.json')
+    }
+})
 function Teste(){
     limpaPagina();
     adicionaDivImagens();
     adicionaLinha();
-if (select.value == 500) {
+if (select.selectedIndex == 1) {
     for (i = 0; i < 6; i++) {
             var num = Math.floor(Math.random() * 50);
-            adicionaColuna(dados[num].Id, dados[num].Url,3);
-            adicionaColuna(dados[num].Id, dados[num].Url,3);
+            adicionaColuna(dados[num].Id, dados[num].Url);
+            adicionaColuna(dados[num].Id, dados[num].Url);
         }
     }
-else if (select.value == 600) {
+else if (select.selectedIndex == 2) {
         for (i = 0; i < 12; i++) {
             var num = Math.floor(Math.random() * 50);
-            adicionaColuna(dados[num].Id, dados[num].Url,3);
-            adicionaColuna(dados[num].Id, dados[num].Url,3);
+            adicionaColuna(dados[num].Id, dados[num].Url);
+            adicionaColuna(dados[num].Id, dados[num].Url);
         }
     } else {
         for (i = 0; i < 24; i++) {
             var num = Math.floor(Math.random() * 50);
-            adicionaColuna(dados[num].Id, dados[num].Url,3);
-            adicionaColuna(dados[num].Id, dados[num].Url,3);
+            adicionaColuna(dados[num].Id, dados[num].Url);
+            adicionaColuna(dados[num].Id, dados[num].Url);
         }
     }   
 }
